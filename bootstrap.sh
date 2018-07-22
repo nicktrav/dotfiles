06c500629543fc0815b6aea48f102009d4676b9c
Nick Travers <n.e.travers@gmail.com>
2018-07-22 23:39:23 +0000

    Update bootstrap script and README
    Allow for specifying a branch and a distro.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,11 @@
 
 # Clones this repo into place and runs setup.sh
 
+ [[ -z "${DISTRO}" ]] && BRANCH='chromeos'
+ [[ -z "${BRANCH}" ]] && DISTRO='master'
+
 mkdir -p ~/Development/dotfiles && \
   cd ~/Development/dotfiles && \
   git clone https://github.com/nicktrav/dotfiles.git . && \
-  git checkout nickt.shellcheck && \
-  ./install.sh "$@"
+  git checkout "$BRANCH" && \
+  ./install.sh "$DISTRO"
